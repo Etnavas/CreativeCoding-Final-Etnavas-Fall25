d2500cd285b09e91e5f4f2e4de19d3c0348be381
Etnavas <km6104@nyu.edu>
2025-11-30 03:18:52 -0500

    Update 2
    Added keyboard sprite movement
    
    Goals/tasks:
    - Decide what core functionality to focus on
    - Figure out what other elements are needed to complete core functions

--- Main/sketch.js
+++ Main/sketch.js
@@ -1,15 +1,47 @@
-let char;
+let player;
 
 function setup() {
 new Canvas ("1:1");
 
-char = new Sprite;
+background (100);
+player = new Sprite;
 //char.diameter = 80;
+player.height = 130;
+player.width = 90;
+
 
 
 
 }
 
 function update() {
+	
 	background (100);
+
+	player.speed = 4.5;
+	
+	//player movement keyboard
+	if (kb.pressing('up')) {
+		player.direction = -90;
+
+		//play animation
+
+	} else if (kb.pressing('down')) {
+		player.direction = 90;
+		
+		//play animation
+
+	} else if (kb.pressing('left')) {
+		player.direction = 180;
+
+		//play animation
+
+	} else if (kb.pressing('right')) {
+		player.direction = 0;
+
+		//play animation
+
+	} else {
+	  player.speed = 0;
+	}
 }
